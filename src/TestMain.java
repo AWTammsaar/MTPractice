d09184a4c9922466af85477ad6ad9a02a4afa738
Al William Tammsaar <TheAoihoshi@gmail.com>
2015-04-12 12:21:33 +0300

    Update TestMain.java

--- src/TestMain.java
+++ src/TestMain.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
  * Created by Al William "Rezo" Tammsaar on 2/25/2015.
  */
 public class TestMain {
-    // Creates a "target" the function is used on, then creates 5 threads, giving them a name and initiating them
+    // Creates a "target" the function is used on, then creates a few threads, giving them a name and initiating them
     public static void main(String[] args) {
         Target target = new Target();
         ArrayList<FunctionThread> threads = new ArrayList<FunctionThread>();
